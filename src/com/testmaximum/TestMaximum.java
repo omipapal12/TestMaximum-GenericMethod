9b94c7db691f899f1d51817a23d313de94bb48ef
omipapal12 <omipapal12@gmail.com>
2021-07-26 19:21:44 +0530

    Added: Maximum Float

--- src/com/testmaximum/TestMaximum.java
+++ src/com/testmaximum/TestMaximum.java
@@ -3,15 +3,15 @@ package com.testmaximum;
 public class TestMaximum {
 public static void main(String[] args) {
 	
-	Integer number1 = 5;
-	Integer number2 = 10;
-	Integer number3 = 15;
-	Integer maximumNum = testMax(number1,number2,number3);
-	System.out.println("Maximum Integer is: " +maximumNum);
+	Float number1 = 5.1f;
+	Float number2 = 10.2f;
+	Float number3 = 15.3f;
+	Float maximumNum = testMax(number1,number2,number3);
+	System.out.println("Maximum Float Number is: " +maximumNum);
 	}
 
-	public static Integer testMax(Integer number1, Integer number2, Integer number3) {
-		Integer maximumnumber = number1;
+	public static Float testMax(Float number1, Float number2, Float number3) {
+		Float maximumnumber = number1;
 		if(number2.compareTo(maximumnumber) > 0);
 		{
 			maximumnumber = number2;
